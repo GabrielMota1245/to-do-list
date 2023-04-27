709534f2569fc916445874c7544b329e4827afcf
GabrielMota1245 <127999585+GabrielMota1245@users.noreply.github.com>
2023-04-26 22:11:00 -0300

    subindo atualizações do arquivo

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,18 +1,42 @@
-import { Button, TextField} from "@mui/material"
+import { Button, TextField } from "@mui/material"
 import "./style.css";
 import { useState } from "react";
 
 function App() {
   const [textTarefa, setTextTarefa] = useState("");
-  function clicarbotao(){
-    console.log(textTarefa)
+  const [listTarefa, setlistTarefa] = useState([]);
+
+
+  function clicarbotao() {
+    setlistTarefa((old) => [...old, textTarefa])
+    setTextTarefa("");
+  }
+  function deletarbotao() {
+  setlistTarefa([])
   }
   return (
-    <form className="form-container">
-     <TextField value={textTarefa} id="standard-basic"  label="Tarefa" variant="filled" placeholder="Digite sua tarefa" onChange={({target}) => setTextTarefa(target.value)} ></TextField>
-     <Button onClick={clicarbotao} variant="contained">Add</Button>
-    </form>
-    
+    <>
+      <form className="form-container">
+        <TextField value={textTarefa} id="standard-basic" label="Tarefa" variant="filled" placeholder="Digite sua tarefa" onChange={({ target }) => setTextTarefa(target.value)} ></TextField>
+        <Button variant="contained" onClick={clicarbotao}>Add</Button>
+        <Button variant="contained" color="secondary" onClick={deletarbotao}>Deletar</Button>
+        {console.log(listTarefa)}
+      </form>
+      <div className="tabela">
+        <div className="cega">  
+          {
+            listTarefa.map((tarefa, index) => (
+              <>
+                <div key={String(index)}>
+                  {tarefa}
+                </div>
+                <div className="divider"/>
+              </>
+            ))
+          }
+        </div>
+      </div>
+    </>
   )
 }
 export default App
